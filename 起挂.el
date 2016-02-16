b0ce6108c3679454648c9ae11ee47e551f36934a
王腾达 <446453149@qq.com>
2016-02-16 23:22:41 +0800

    修改显示结果，简单增加变卦的表示

--- 起挂.el
+++ 起挂.el
@@ -3,22 +3,31 @@
                 (delq (assoc key l) l))))
 (defvar 六个爻 '("初爻" "二爻" "三爻" "四爻" "五爻" "上爻"))
 
-(defvar 老阳)
-(defvar 老阴)
-(defvar 少阳)
-(defvar 少阴)
+(defvar 老阳 "老阳 ———  ->  —  —")
+(defvar 老阴 "老阴 —  —  ->  ———")
+(defvar 少阳 "少阳 ———      ———")
+(defvar 少阴 "少阴 —  —      —  —")
 
-(defvar f '((1 . 少阳) (2 . 少阴)(3 . 老阳) (0 . 老阴)))
+(defvar 四相 '((1 . 少阳) (2 . 少阴)(3 . 老阳) (0 . 老阴)))
+
+(defvar 挂)
+
+(defun 装挂 ()
+  (setq 挂 nil)
+  (dolist (n 六个爻)
+    (setq 挂 (cons (cons n (cdr (assoc (random 4) 四相)))
+		   挂))))
+
+(defun 新buffer显示挂 ()
+  (with-current-buffer
+      (switch-to-buffer "新挂")
+    (dolist (每爻 挂)
+      (insert (concat (symbol-value (cdr 每爻)))
+	      "\n"))))
 
 (defun 起挂 ()
   (interactive)
-  (if 挂
-      (setq 挂 nil)
-      (defvar-local 挂 nil))
-  (dolist (n 六个爻)
-    (setq 挂 (cons (cons n (cdr (assoc (random 4) f)))
-		   挂)))
-  (with-current-buffer "新挂"
-    (dolist (爻 挂)
-      (insert (concat (symbol-name (cdr 爻)) "\n")))
-    (message 挂)))
+
+  (装挂)
+
+  (新buffer显示挂))
